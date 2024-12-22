08792733e690315e2d4849f763e47f210185a793
Tsvetelina867 <cvettyasd@gmail.com>
2024-12-23 01:07:14 +0200

    Added login endpoint and jwt token for authentication

--- src/main/java/com/example/spring/finance/controller/AuthController.java
+++ src/main/java/com/example/spring/finance/controller/AuthController.java
@@ -1,9 +1,12 @@
 package com.example.spring.finance.controller;
 
+import com.example.spring.finance.dtos.LoginRequest;
 import com.example.spring.finance.dtos.RegistrationRequest;
 import com.example.spring.finance.service.AuthService;
+import com.example.spring.finance.util.JwtUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +16,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthController {
     private final AuthService authService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
 
-    public AuthController(AuthService authService) {
+    public AuthController(AuthService authService, AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
         this.authService = authService;
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
+        String result = authService.loginUser(loginRequest.getUsername(), loginRequest.getPassword());
+
+        if (result.startsWith("Bearer")) {
+            return ResponseEntity.ok(result);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+        }
     }
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody RegistrationRequest registrationRequest) {
