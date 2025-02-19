f28e5dc9af803032442320883b287a55f7d52570
Tsvetelina867 <cvettyasd@gmail.com>
2025-02-19 14:04:20 +0200

    Added fallback message for no data for the chart and added ui for api documentation

--- frontend/src/components/SpendingChart.js
+++ frontend/src/components/SpendingChart.js
@@ -1,6 +1,22 @@
 import { LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid, ResponsiveContainer } from 'recharts';
 
 const SpendingChart = ({ data }) => {
+    if (!data || data.length === 0) {
+        return (
+          <p style={{
+            fontSize: '1.2rem',
+            color: '#777',
+            textAlign: 'center',
+            padding: '20px',
+            backgroundColor: '#f9f9f9',
+            border: '1px solid #ccc',
+            borderRadius: '8px',
+            marginTop: '20px'
+          }}>
+            No data available for chart.
+          </p>
+        );
+      }
     return (
         <ResponsiveContainer width="100%" height={300}>
             <LineChart data={data}>
